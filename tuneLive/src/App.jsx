fe722541c2670dd775b6693611c417d0d42b7bda
Cassius <111204494+cass-us@users.noreply.github.com>
2024-08-21 08:56:16 +0200

    h3

--- tuneLive/src/App.jsx
+++ tuneLive/src/App.jsx
@@ -1,11 +1,13 @@
 
 import NavBar from "./components/NavBar.jsx";
 import LatestMusic from "./components/LatestMusic.jsx";
+import Footer from "./components/Footer.jsx";
 const App = () => {
   return (
    <>
     <NavBar/>
     <LatestMusic/>
+    <Footer/>
    </>
   )
 }
